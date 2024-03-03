0422d01542ed46f748db766e490c81a7335be6de
vr1 <hivr1eva@gmail.com>
2024-03-03 12:24:34 -0500

    update course thread types

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,57 @@
-export type Course = {
+import { ThreadMessage } from "openai/resources/beta/threads/index.mjs"
+import { object } from "zod"
+
+export type ActionErrorResponse = null | string
+
+export type Thread = {
     id: string
 }
 
-export type CreateCourseResponse = {
-    error: null | string,
-    course?: Course
+export type Course = {
+    id: string,
+    object: string,
+    created_at: number,
+    metadata: CourseMetadata
+}
+
+export type CourseMetadata = {
+    title: string,
+    speaker: string,
+    avatar: string,
+    organization: string,
+    thumbnail: string,
+    duration: string,
+    cc: boolean,
+    description: string
+}
+
+export type CreateThreadResponse = {
+    error: ActionErrorResponse
+    thread?: Thread
+}
+
+export interface CreateThreadArgs {
+    title: string
+}
+
+export type FetchCoursesResponse = {
+    courses?: ThreadMessage[],
+    error: ActionErrorResponse
+}
+
+export interface CourseListProps {
+    courses: ThreadMessage[]
 }
 
 export interface CreateCourseArgs {
     title: string
+}
+
+export type CreateCourseResponse = {
+    course?: ThreadMessage
+    error: ActionErrorResponse
+}
+
+export interface CoursePreviewProps {
+    course: ThreadMessage
 }
