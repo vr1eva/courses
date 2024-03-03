ee5bb45af2ae90e3adc515a4434fb968bce79480
vr1 <hivr1eva@gmail.com>
2024-03-03 14:00:54 -0500

    embed figjam board in bootcamp page

--- src/app/bootcamp/page.tsx
+++ src/app/bootcamp/page.tsx
@@ -7,6 +7,9 @@ export default async function Bootcamp() {
     }
 
     return (
-        <div>Bootcamp</div>
+        <div className="flex items-center flex-col pt-7">
+            <iframe style={{ border: "1px solid rgba(0, 0, 0, 0.1);" }} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/file/G7fV8n7wYSHYOFmlTY09TH/cursors?type=whiteboard&node-id=1-4&t=9glg1LTgRP7ly4h1-4" allowFullScreen>
+            </iframe>
+        </div>
     )
 }
